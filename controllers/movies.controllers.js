0837fedaea4500ac222d97282da955ab67d244ce
priyanka <priyankapriya57037@gmail.com>
2024-10-01 21:43:49 +0530

    add controller files

---
+++ controllers/movies.controllers.js
@@ -0,0 +1,4 @@
+export const MovieIndex= (req, res) => {res.send("Get All Movies List");}
+export const MovieCreate= (req, res) => {res.send("Create a movie");}
+export const MovieUpdate =(req, res) => {res.send("Update a movie");}
+export const MovieDelete = (req, res) => {res.send("Delete a movie");}

16677a5a1504d8722c272f034f64a6ca66bdfadd
priyanka <priyankapriya57037@gmail.com>
2024-10-01 20:30:49 +0530

    route structure

--- main.js
+++ main.js
@@ -1,14 +1,34 @@
 import express from "express";
 
-const app =express();
-const PORT=8000;
+const app = express();
+const PORT = 8000;
 
-app.get("/",(req,res)=>{
-    res.json({msg:"Hello Students!"});
+app.get("/", (req, res) => {
+    res.json({ msg: "Hello Students!" });
+
+})
+// crud functionality for papa
+
+// R- For Reading
+app.get("/movies", () => {
+
+})
+// C- For Creating
+app.post("/movies", () => {
+
+})
+// U- For Updating Movie
+app.put("/movies/:id", () => {
+
+})
+// D -For Deleting Movie
+app.delete("/movies/:id", () => {
 
 })
 
-app.listen(PORT,()=>{
+
+
+app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
-    
+
 })
